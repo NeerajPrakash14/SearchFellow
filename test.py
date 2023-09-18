58cf3117cc266a9c13051e280497094abf8ddcf8
Neeraj Prakash <neerajprakashreddy@gmail.com>
2023-09-18 23:04:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -106,3 +106,41 @@ print("global - after-> ", id(test), test)
 # function(test)
 # print(test)  # This will print the updated global variable 'test', which is now 2
 # print("global - after-> ", id(test), test)
+
+
+
+# NGINX.CONF
+
+# # worker_processes  1;
+
+# # events {
+# #     worker_connections  1024;
+# # }
+
+
+# server {
+#     large_client_header_buffers 4 32k;
+#     listen 80;
+#     server_name  localhost;
+
+#     # root   /data/www;
+#     # index  index.html;
+#     # include /etc/nginx/mime.types;
+
+#     # gzip on;
+#     # gzip_min_length 1000;
+#     # gzip_proxied expired no-cache no-store private auth;
+#     # gzip_types text/plain text/css application/json application/javascript application/x-javascript text/xml application/xml application/xml+rss text/javascript;
+
+#     # location / {
+#     #     try_files $uri $uri/ /index.html =404;
+#     # }
+#     location / {
+#         proxy_pass http://0.0.0.0:8002;
+#         proxy_set_header Host $http_host;
+#         proxy_redirect off;
+#         proxy_http_version 1.1;
+#         # proxy_set_header Upgrade $http_upgrade;
+#         # proxy_set_header Connection "upgrade";
+#     }
+# }
